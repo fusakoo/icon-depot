ef2a9d9a332266e7de1e565fff65e9441dc2f01f
fusakoo <obatafusako@gmail.com>
2022-02-24 00:15:00 -0800

    Add some documentations

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import requests
 import json
 
 @app.route('/')
 def index():
-    return 'This is the endpoint for icon-depot microservice.'
+    '''
+    Index page
+    '''
+    return render_template('index.html')
 
 @app.route('/icons-available')
 def get_icon_available():
+    '''
+    Get a list of icons available by checking the materials icon metadata
+    '''
     r = requests.get('https://fonts.google.com/metadata/icons')
     rtext = r.text[5:]
     with open('app/static/icon_list.json', 'w') as json_file:
@@ -26,18 +32,39 @@ def get_icon_available():
             icon_dict['icons'].append(entry)
     return icon_dict
 
-@app.route('/icon/<icon_name>')
-def get_icon(icon_name):
+def check_if_available(icon_name):
+    '''
+    Checks if the name exists in the available list of icons
+    '''
     icon_dict = get_icon_available()
     icons = icon_dict['icons']
     for icon in icons:
         name = icon.get('name')
         if name == icon_name:
-            output = {
-                'html' : '<span class="material-icons">{}</span>'.format(icon_name)
-            }
-            return output
+            return True
+    return False
+
+@app.route('/icon/<icon_name>')
+def get_icon(icon_name):
+    '''
+    Checks available icons & returns a dict containing an html for the icon
+    '''
+    if check_if_available(icon_name):
+        output = {
+            'html' : '<span class=\"material-icons\">{}</span>'.format(icon_name)
+        }
+        return output
 
 @app.route('/icons', methods=['POST'])
-def get_icons_json():
-    return 'Json page'
+def icons_json():
+    '''
+    Checks available icons & returns a dict containing list of html for the icons
+    '''
+    request_data = request.get_json()
+    icon_names = request_data['icon_list']
+    output = { 'html': [] }
+    for icon_name in icon_names:
+        if check_if_available(icon_name):
+            icon_html = '<span class=\"material-icons\">{}</span>'.format(icon_name)
+            output['html'].append(icon_html)
+    return output
