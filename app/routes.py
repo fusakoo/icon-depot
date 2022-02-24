62dbd3c7ec85d61a6f1eb1a44d44e3052b86d64c
fusakoo <obatafusako@gmail.com>
2022-02-23 23:06:46 -0800

    Get the single icon request working

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ import json
 def index():
     return 'This is the endpoint for icon-depot microservice.'
 
-@app.route('/icon-list')
-def get_icon_list():
+@app.route('/icons-available')
+def get_icon_available():
     r = requests.get('https://fonts.google.com/metadata/icons')
     rtext = r.text[5:]
     with open('app/static/icon_list.json', 'w') as json_file:
@@ -17,11 +17,27 @@ def get_icon_list():
     with open('app/static/icon_list.json') as json_file:
         data = json.load(json_file)
         icons = data['icons']
-        icon_list = {'icons': []}
+        icon_dict = {'icons': []}
         for icon in icons:
             entry = {
                 'name' : icon['name'],
                 'categories' : icon['categories']
             }
-            icon_list['icons'].append(entry)
-    return icon_list
+            icon_dict['icons'].append(entry)
+    return icon_dict
+
+@app.route('/icon/<icon_name>')
+def get_icon(icon_name):
+    icon_dict = get_icon_available()
+    icons = icon_dict['icons']
+    for icon in icons:
+        name = icon.get('name')
+        if name == icon_name:
+            output = {
+                'html' : '<span class="material-icons">{}</span>'.format(icon_name)
+            }
+            return output
+
+@app.route('/icons', methods=['POST'])
+def get_icons_json():
+    return 'Json page'
