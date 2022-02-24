afb490a97edabbdbeca160b6fc9e778f2183ebb1
fusakoo <obatafusako@gmail.com>
2022-02-23 16:51:02 -0800

    Get the icon list page working

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,27 @@
 from app import app
+from flask import render_template
+import requests
+import json
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return 'This is the endpoint for icon-depot microservice.'
+
+@app.route('/icon-list')
+def get_icon_list():
+    r = requests.get('https://fonts.google.com/metadata/icons')
+    rtext = r.text[5:]
+    with open('icon_list.json', 'w') as json_file:
+        json_file.write(rtext)
+        json_file.close
+    with open('icon_list.json') as json_file:
+        data = json.load(json_file)
+        icons = data['icons']
+        icon_list = {'icons': []}
+        for icon in icons:
+            entry = {
+                'name' : icon['name'],
+                'categories' : icon['categories']
+            }
+            icon_list['icons'].append(entry)
+    return icon_list
